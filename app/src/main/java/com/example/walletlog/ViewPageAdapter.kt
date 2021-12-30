383f8ef17b28e64113449f7e7691b4225fcf010b
Mikhail Chuduk <chuduk@firecode.ru>
2021-12-30 08:54:24 +0300

    version 1.0

---
+++ app/src/main/java/com/example/walletlog/ViewPageAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.walletlog
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class VPAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+    
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+}
